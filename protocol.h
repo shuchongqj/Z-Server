9f17e52dad79e07a876ff248802e3c8d810042f9
Intueor <procyon1980@mail.ru>
2016-11-22 00:29:41 +0300

    Дальнейшее развитие по работе с клиентом.

--- protocol.h
+++ protocol.h
@@ -7,11 +7,13 @@
 // Утиль.
 #define PObjNaming(name)				o##name // Создаёт имя объекта структуры добавлением 'o' в начале.
 #define PObjDecl(name)					name PObjNaming(name) // Унифицированная декларация объектов.
+// Определение структуры члена протокола с именем структуры, определением данных и авто-добавлением объекта с предикатом 'o'.
 #define ProtocolStorageDef(name, code)	struct name															\
 										{																	\
 											code															\
 										};																	\
 										PObjDecl(name)
+// Определение класса протокола с перечислением структур типа 'ProtocolStorageDef'.
 #define ProtocolClassInit(defs)			class ProtocolStorage												\
 										{																	\
 										public:																\
@@ -19,13 +21,16 @@
 										}
 
 // ========================= КОДЫ ВЗАИМОДЕЙСТВИЯ ==============================
-#define PROTO_C_SEND_PASSW			'a'
-#define PROTO_S_PASSW_OK			'b'
-#define PROTO_S_PASSW_ERR			'c'
+#define PROTO_C_SEND_PASSW			'H'
+#define PROTO_S_PASSW_OK			'P'
+#define PROTO_S_PASSW_ERR			'p'
+#define PROTO_C_REQUEST_LEAVING		'L'
+#define PROTO_S_ACCEPT_LEAVING		'B'
+#define PROTO_S_SHUTDOWN_INFO		'Q'
 // ============================================================================
 
 // ============================ КОДЫ ПАКЕТОВ ==================================
-#define PROTO_O_TEXT_MSG			't'
+#define PROTO_O_TEXT_MSG			'T'
 // ============================================================================
 
 ProtocolClassInit(
@@ -33,8 +38,8 @@ ProtocolClassInit(
 // ====================== ОБЪЯВЛЕНИЯ СТРУКТУР ПАКЕТОВ =========================
 	ProtocolStorageDef
 	(
-		TextMsg,
-		char m_chMsg[MAX_MSG];
+		TextMsg, // Имя типа структуры.
+		char m_chMsg[MAX_MSG]; // Данные.
 	);
 // ============================================================================
 
--- protoparser.h
+++ protoparser.h
@@ -5,27 +5,30 @@
 #include "protocol.h"
 
 //== МАКРОСЫ.
-#define PROTOPARSER_COMMAND			0x00
-#define PROTOPARSER_OK				0x01
-#define PROTOPARSER_OUT_OF_RANGE	0x02
-#define PROTOPARSER_UNKNOWN_COMMAND	0x03
+#define PROTOPARSER_OK				0x00
+#define PROTOPARSER_OUT_OF_RANGE	0x01
+#define PROTOPARSER_UNKNOWN_COMMAND	0x02
 
 //== КЛАССЫ.
 /// Класс парсера протокола.
 class ProtoParser
 {
 public:
+	/// Структура описания и хранинлища.
 	struct ParsedObject
 	{
-		char chTypeCode;
-		int iDataLength;
-		ProtocolStorage oProtocolStorage;
+		char chTypeCode; ///< Тип пакета.
+		int iDataLength; ///< Длина пакета в байтах.
+		ProtocolStorage oProtocolStorage; ///< Составной объект хранилища, определяемый в протоколе.
 	};
 public:
-	ParsedObject oParsedObject;
+	ParsedObject oParsedObject; ///< Объект структуры описания и хранилище.
 public:
-	/// Парсинг пакета.
+	/// Парсинг пакета в соответствующий член хранилища.
 	char ParsePocket(char* p_chData, int iLength);
+											///< \param[in] p_chData Указатель на пакет.
+											///< \param[in] iLength Длина пакета в байтах.
+											///< \return Результат парсинга.
 };
 
 #endif // PROTOPARSER_H
